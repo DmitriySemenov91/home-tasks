1377dee5d0a7b751111ca6f8a854b1967c25f67e
dimka <dimka8445484@gmail.com>
2022-06-23 16:24:40 +0200

    added: task-6-5

--- src/task-6-2/pages/index.tsx
+++ src/task-6-2/pages/index.tsx
@@ -1,6 +1,15 @@
 import React, { useState } from "react";
 
 import { $api } from "../http";
+import axios from "axios";
+
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import * as yup from "yup";
 
 interface IUsers {
   id: string;
@@ -8,6 +17,12 @@ interface IUsers {
   name: string;
   avatar: string;
   carNames: string;
+  age: number;
+}
+
+interface Inputs {
+  name: string;
+  age: number;
 }
 
 export const Users: React.FC<{}> = () => {
@@ -29,22 +44,113 @@ export const Users: React.FC<{}> = () => {
     }
   };
 
+  // POST
+  const FormSchema = yup.object({
+    name: yup.string(),
+    age: yup.number(),
+  });
+
+  // react-hook-form variant form
+  const { handleSubmit, control } = useForm<Inputs>({
+    resolver: yupResolver(FormSchema),
+  });
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    try {
+      await $api.post("/users", data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const sendFile = async () => {
+    const fileElem = document.querySelector("#file") as HTMLInputElement | null;
+
+    const fileItem = fileElem?.files;
+    const formData = new FormData();
+
+    if (fileItem) {
+      formData.append("file", fileItem[0]);
+    }
+    await axios.post("http://localhost:9999", formData, {
+      headers: { "Content-type": "multipart/form-data" },
+    });
+  };
+
   return (
     <>
       <div>{loading && "Loading....."}</div>
       <div>{error}</div>
       <div>
-        <button onClick={getData}>get list of users</button>
-        {users.map(({ id, publicDate, name, avatar, carNames }: IUsers) => (
-          <div key={id}>
-            <div>Date: {publicDate}</div>
-            <div>Name: {name}</div>
-            <div>
-              <img src={avatar || ""} alt={name} data-testid="avatar" />
+        <input id="file" type="file" />
+        <input type="button" onClick={sendFile} value="send file" />
+      </div>
+      <>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="formWrapper">
+            <div className="formItem">
+              <Controller
+                render={({ field }) => (
+                  <TextField
+                    variant="outlined"
+                    fullWidth
+                    label="name"
+                    placeholder="name"
+                    {...field}
+                  />
+                )}
+                control={control}
+                name="name"
+                defaultValue=""
+              />
             </div>
-            <div>Car: {carNames}</div>
+            <div className="formItem">
+              <Controller
+                render={({ field }) => (
+                  <TextField
+                    variant="outlined"
+                    fullWidth
+                    label="age"
+                    placeholder="age"
+                    {...field}
+                  />
+                )}
+                control={control}
+                name="age"
+                defaultValue={0}
+              />
+            </div>
           </div>
-        ))}
+          <div className="formWrapper">
+            <span className="button">
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                endIcon={<NavigateNextIcon />}
+                name="second step"
+              >
+                Send
+              </Button>
+            </span>
+          </div>
+        </form>
+      </>
+      <div>
+        <button onClick={getData}>get list of users</button>
+        {users.map(
+          ({ id, publicDate, name, avatar, carNames, age }: IUsers) => (
+            <div key={id}>
+              <div>Date: {publicDate}</div>
+              <div>Name: {name}</div>
+              <div>
+                <img src={avatar || ""} alt={name} data-testid="avatar" />
+              </div>
+              <div>Car: {carNames}</div>
+              <div>Age: {age}</div>
+            </div>
+          )
+        )}
       </div>
     </>
   );
